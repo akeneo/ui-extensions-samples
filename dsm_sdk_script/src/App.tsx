9eb510f1225bc2e39bcd1c542230c09f3c448dfc
Pierre Allard <pierallard@gmail.com>
2024-09-26 16:03:32 +0200

    PROGX-306: Add DSM

--- dsm_sdk_script/src/App.tsx
+++ dsm_sdk_script/src/App.tsx
@@ -1,5 +1,14 @@
+import {Link, Placeholder, UsersIllustration} from "akeneo-design-system";
+
 function App() {
-  return <div>Test</div>
+  return <Placeholder
+      size={'large'}
+      illustration={<UsersIllustration />}
+      title="Welcome to the SDM SDK starter kit!"
+  >
+    Please update the src/App.tsx file to match your needs!<br/>
+    <Link href="https://dsm.akeneo.com/" target="_blank">Link to the official Akeneo DSM</Link>
+  </Placeholder>
 }
 
 export default App
--- dsm_sdk_script/src/main.tsx
+++ dsm_sdk_script/src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.tsx'
 import './index.css'
+import {pimTheme} from "akeneo-design-system";
+import {ThemeProvider} from "styled-components";
 
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={pimTheme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
   document.getElementById('root')
 );

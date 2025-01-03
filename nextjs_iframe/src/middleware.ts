32b11c392b28ad3a4cc350531a3a7cb7ad75a8fa
Aymeric Planche <aymeric.planche@akeneo.com>
2025-01-03 10:13:33 +0100

    define proper csp policy

--- nextjs_iframe/src/middleware.ts
+++ nextjs_iframe/src/middleware.ts
@@ -1,15 +1,41 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextRequest } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-    if(!request.cookies.has('weak-session'))
+
+    if(request.nextUrl.pathname.startsWith('/private/dashboard') && !request.cookies.has('weak-session'))
     {
         return NextResponse.redirect(new URL('/private/login', request.url))
     }
-}
- 
-// See "Matching Paths" below to learn more
-export const config = {
-    matcher: '/private/dashboard/:path*',
+
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64')
+    const cspHeader = `
+      frame-ancestors 'self' http://localhost:8080;
+      upgrade-insecure-requests;
+  `
+    // Replace newline characters and spaces
+    const contentSecurityPolicyHeaderValue = cspHeader
+      .replace(/\s{2,}/g, ' ')
+      .trim()
+   
+    const requestHeaders = new Headers(request.headers)
+    requestHeaders.set('x-nonce', nonce)
+   
+    requestHeaders.set(
+      'Content-Security-Policy',
+      contentSecurityPolicyHeaderValue
+    )
+   
+    const response = NextResponse.next({
+      request: {
+        headers: requestHeaders,
+      },
+    })
+    response.headers.set(
+      'Content-Security-Policy',
+      contentSecurityPolicyHeaderValue
+    )
+
+    return response
 }

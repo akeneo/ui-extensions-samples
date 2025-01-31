c1cf106ea75759daa2c9cb4f88a1f8554c0e6909
Aymeric Planche <aymeric.planche@akeneo.com>
2025-01-31 13:21:13 +0000

    add simple endpoints for testing ui extension type action

---
+++ nextjs_iframe/pages/api/index.tsx
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+ 
+type ResponseData = {
+  message: string
+  endpoints: object
+}
+ 
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+    res.status(200).json({
+      message: 'Use following endpoints for testing purpose',
+      endpoints: { 
+        success: '/api/success',
+        error: '/api/error',
+        timeout: '/api/wait_for_height_seconds',
+      } 
+    })
+}
